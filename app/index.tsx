ccb710c00a8c4bf1f38e2ad7971044211d9521fa
QuentinNev <neves.quentin@gmail.com>
2025-08-25 16:50:05 +0200

    Install and test skia

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { Canvas, Path, Skia, SkPath } from "@shopify/react-native-skia";
+import React, { useState } from "react";
+import { PanResponder, View } from "react-native";
 
-export default function Index() {
+export default function App() {
+  const [paths, setPaths] = useState<SkPath[]>([]);
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: ({ nativeEvent }) => {
+      const newPath = Skia.Path.Make();
+      newPath.moveTo(nativeEvent.locationX, nativeEvent.locationY);
+      setPaths([...paths, newPath]);
+    },
+    onPanResponderMove: ({ nativeEvent }) => {
+      const updatedPaths = [...paths];
+      updatedPaths[updatedPaths.length - 1].lineTo(
+        nativeEvent.locationX,
+        nativeEvent.locationY
+      );
+      setPaths(updatedPaths);
+    },
+  });
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1 }} {...panResponder.panHandlers}>
+      <Canvas style={{ flex: 1 }}>
+        {paths.map((p, i) => (
+          <Path key={i} path={p} color="black" style="stroke" strokeWidth={4} />
+        ))}
+      </Canvas>
     </View>
   );
 }
