d71d3dfc8c3134a2a2c2416dd7a75d0dab037501
QuentinNev <neves.quentin@gmail.com>
2025-08-25 17:07:29 +0200

    Remove unused SkPath import and Stroke type from Home component

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,9 +1,6 @@
 import DrawingCanvas from "@/components/DrawingCanvas";
-import { SkPath } from "@shopify/react-native-skia";
 import React from "react";
 import { View } from "react-native";
-
-type Stroke = { path: SkPath; color: string; strokeWidth: number };
 
 export default function Home() {
   return (
